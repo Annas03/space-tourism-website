8e4dffd2446a519d3bb0f695c92de8894c1c53c9
Annas03 <baigannas9@gmail.com>
2022-06-14 22:20:56 +0500

    crew-tablet-responsive

--- starter-code/technology.js
+++ starter-code/technology.js
@@ -3,10 +3,13 @@ let about = document.getElementById("about-technology")
 let img = document.getElementById("tech-image")
 let navBar = document.querySelector(".mobile-navbar")
 let body = document.getElementsByTagName("body")
+let menu = document.getElementById("mobile-menu")
 let images = [];
 
 let active = "vehicle"
 let menuActive = false
+
+let styleElem = document.head.appendChild(document.createElement("style"));
 
 function changeData(n){
     fetch("data.json")
@@ -21,7 +24,7 @@ function changeData(n){
         }
         else{
             for(let i = 0; i < 3; i++){
-                img.src = main["technology"][i]["images"]["landscape"]
+                // img.src = main["technology"][i]["images"]["landscape"]
                 images.push(main["technology"][i]["images"]["landscape"])
             }
         }
@@ -81,12 +84,22 @@ function DisplayMenu(){
         navBar.style.display = "block"
         document.body.style.height = "100%"
         document.body.style.overflowY = "hidden"
+        ChangeMobileMenu()
     } 
     else{
         navBar.style.display = "none"
         document.body.style.overflowY = "auto"
+        UnChangeMobileMenu()
     }
     menuActive = !menuActive;
+}
+
+function ChangeMobileMenu(){
+    styleElem.innerHTML = "#mobile-menu{margin-top:1rem;margin-right:1rem;transform:rotate(45deg);}#mobile-menu::after{left:0;bottom:0;transform:rotate(-92.5deg);}#mobile-menu::before{display:none;}"
+}
+
+function UnChangeMobileMenu(){
+    styleElem.innerHTML = "#mobile-menu::before{display:span;}"
 }
 
 VehicleSelected()
